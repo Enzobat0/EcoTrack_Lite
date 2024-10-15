0226a2320b5cb9cfb9983aaf89478c503e532c18
Enzo Bat <e.batungwan@alustudent.com>
2024-10-15 18:58:18 +0200

    launch_screen

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LaunchScreen extends StatelessWidget {
+  const LaunchScreen({super.key});
+  static const Color buttoncolor = Color(0xff34A353);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Image(image: AssetImage('images/logo-white.png')),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: buttoncolor,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40, vertical: 22),
+                textStyle: GoogleFonts.dmSans(
+                  textStyle: const TextStyle(fontSize: 26,), 
+                ),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+              ),
+              
+              child: const Text('Footprint Calculator'),
+              ),
+
+              const SizedBox(height: 50,),
+            TextButton(
+              onPressed: (){}, 
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: GoogleFonts.dmSans(
+                  textStyle: const TextStyle(fontSize: 16,), 
+                ),
+              ),
+              child: const Text('Already a member? Login'),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
